37d16c2aafcfd6d64c46f7d9a67a97a0c876d35e
Damien de Lartigue <damien@driving.vision>
2021-03-19 22:24:57 +0000

    add ingredients index route

--- cookbook-backend/app/controllers/ingredients_controller.rb
+++ cookbook-backend/app/controllers/ingredients_controller.rb
@@ -1,5 +1,7 @@
 class IngredientsController < ApplicationController
   def index
+    ingredients = Ingredient.all
+    render json: IngredientSerializer.new(ingredients)
   end
 
   def show
--- cookbook-frontend/index.js
+++ cookbook-frontend/index.js
@@ -1,30 +1,47 @@
 $(document).foundation();
 
 document.addEventListener('DOMContentLoaded', function(){
+	getRecipes();
+});
+
+function getRecipes(){
 	fetch('http://localhost:3000/recipes')
 		// Handle success
 	    .then(response => response.json())  // convert to json
-	    .then(json => recipeIndex(json))    //print data to console
-	    .catch(err => console.log('Request Failed', err)); // Catch errors
-})
+	    .then(json => index(json, document.querySelector("#recipes-index").innerHTML))    //print data to console
+	    .catch(err => console.log('Request Failed', err)); // Catch errors	
+};
 
-function recipeIndex(json){
-	const source = document.querySelector("#recipes").innerHTML;
+function getIngredients(){
+	fetch('http://localhost:3000/ingredients')
+		// Handle success
+	    .then(response => response.json())  // convert to json
+	    .then(json => index(json, document.querySelector("#ingredients-index").innerHTML))    //print data to console
+	    .catch(err => console.log('Request Failed', err)); // Catch errors	
+}
 
+function index(json, source){
   // compile it using Handlebars
   const template = Handlebars.compile(source);
   let html = "";
 
   for (let i = 0; i < json.data.length; i++){
-
 	  // get the HTML after passing the template the context
 	  html += template(json.data[i]);
-
 	};
-  // debugger;
   // get the element to set the new HTML into
   const destination = document.querySelector("#content");
 
   // set the new HTML
   destination.innerHTML = html;
-}
+};
+
+document.querySelector("#recipes").addEventListener('click', function(){
+	getRecipes();
+	$('#nav-menu').foundation('close');
+});
+
+document.querySelector("#ingredients").addEventListener('click', function(){
+	getIngredients();
+	$('#nav-menu').foundation('close');
+});
