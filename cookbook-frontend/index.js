e9c678a48b4b54c98f0bb732fbc23ec1458ab8c4
Damien de Lartigue <damien@driving.vision>
2021-03-19 21:51:28 +0000

    recipes index path is operational

--- cookbook-frontend/index.js
+++ cookbook-frontend/index.js
@@ -2,4 +2,29 @@ $(document).foundation();
 
 document.addEventListener('DOMContentLoaded', function(){
 	fetch('http://localhost:3000/recipes')
+		// Handle success
+	    .then(response => response.json())  // convert to json
+	    .then(json => recipeIndex(json))    //print data to console
+	    .catch(err => console.log('Request Failed', err)); // Catch errors
 })
+
+function recipeIndex(json){
+	const source = document.querySelector("#recipes").innerHTML;
+
+  // compile it using Handlebars
+  const template = Handlebars.compile(source);
+  let html = "";
+
+  for (let i = 0; i < json.data.length; i++){
+
+	  // get the HTML after passing the template the context
+	  html += template(json.data[i]);
+
+	};
+  // debugger;
+  // get the element to set the new HTML into
+  const destination = document.querySelector("#content");
+
+  // set the new HTML
+  destination.innerHTML = html;
+}
