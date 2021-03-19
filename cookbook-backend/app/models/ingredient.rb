50936ae541ce7484fbca7e96a04d1f6594157e7b
Damien de Lartigue <damien@driving.vision>
2021-03-19 14:01:24 +0000

    add basic server side validations

--- cookbook-backend/app/models/ingredient.rb
+++ cookbook-backend/app/models/ingredient.rb
@@ -1,3 +1,5 @@
 class Ingredient < ApplicationRecord
 	has_and_belongs_to_many :recipes
+
+	validates_presence_of :name, :calories, :protein, :fat, :carb
 end
--- cookbook-backend/app/models/recipe.rb
+++ cookbook-backend/app/models/recipe.rb
@@ -1,3 +1,6 @@
 class Recipe < ApplicationRecord
 	has_and_belongs_to_many :ingredients
+
+	validates_presence_of :name, :instructions
+	validates_uniqueness_of :name
 end
